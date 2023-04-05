9cafb3d048ff7459a4df4ad5d56235c11c93fe84
AigerimSam <aigerimsam2692@gmail.com>
2023-04-05 07:45:49 +0300

    Initial commit

---
+++ src/main/java/com/example/rentcarsapp/RentCarsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentcarsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentcarsapp/RentCarsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentcarsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
